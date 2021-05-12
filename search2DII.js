568a9cb526a9d8e6ef0b3b4f702a31bcdf88b5b4
lxy878 <lxy878@msn.cn>
2021-05-12 08:43:26 -0400

    Modify search 2D II

--- search2DII.js
+++ search2DII.js
@@ -14,8 +14,8 @@
 // output: true
 
 function search2DII(matrix, target){
-    const m = matrix.length-1
-    let i = matrix[0].length-1
+    const m = matrix[0].length-1
+    let i = matrix.length-1
     let j = 0
 
     while(i>=0 && j<=m){
@@ -36,3 +36,5 @@ console.log(search2DII(
     [3,   6,  9, 16, 22],
     [10, 13, 14, 17, 24],
     [18, 21, 23, 26, 30]], 14))
+
+console.log(search2DII([[1,1,]], 14))
